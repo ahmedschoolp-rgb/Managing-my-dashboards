fe160b667e3aeb206134091831d59c63c8ab6e9f
ahmedschoolp-rgb <ahmedschoolp@gmail.com>
2026-08-06 23:47:59 +0300

    feat: add push notification permission request
    Request notification permissions upon user interaction and login to prepare for future alert functionality, and implement click handling in the service worker.

--- sw.js
+++ sw.js
@@ -9,3 +9,20 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((windowClients) => {
+      for (let i = 0; i < windowClients.length; i++) {
+        let client = windowClients[i];
+        if (client.url.indexOf('/') !== -1 && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
